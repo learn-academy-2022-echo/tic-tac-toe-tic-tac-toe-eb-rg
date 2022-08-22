ec88fc7b573dd38a9b7c93161f7e6134d0850c13
ElijahBcodes <eli1inthesnow@gmail.com>
2022-08-22 11:05:29 -0700

    first-story

--- src/App.js
+++ src/App.js
@@ -4,11 +4,58 @@ import './App.css'
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
-
+  const [currentPlayer, setCurrentPlayer] = useState("❌") 
+  const handleClick = (index) => {
+    alert(index)    
+} 
+  // const [winner, setWinner] =useState(null)
+  // const playerSelection = (currentClick) =>{
+  //   if(squares[currentClick] === null && !winner){
+  //     squares[currentClick] = currentPlayer
+  //     setSquares(squares)
+  //     setCurrentPlayer(currentPlayer === "❌" ? "⭕️" : "❌")
+  //   }
+    // winning()
+  // }
+  // const winningConditions = [
+  //   [0, 1, 2],
+  //   [3, 4, 5],
+  //   [6, 7, 8],
+  //   [0, 3, 6],
+  //   [1, 4, 7],
+  //   [2, 5, 8],
+  //   [0, 4, 8],
+  //   [2, 4, 6],
+  // ]
+//   winningConditions.forEach(value => {
+//     const [first, second, third] = value
+//     if(
+//       squares[first] === "❌" &&
+//       squares[second] === "❌" &&
+//       squares[third] === "❌"
+//     ){
+//       setWinner("❌")
+//     } else if(
+//       squares[first] === "⭕️" &&
+//       squares[second] === "⭕️" &&
+//       squares[third] === "⭕️"
+//     ){
+//       setWinner("⭕️")
+//     }
+//   })
+// }
   return (
     <>
       <h1>Tic Tac Toe</h1>
-      <Square />
+      <div className= "boardgame">
+      {squares.map((value, index) => {
+              return(
+                <Square value ={value} index={index} handleClick={handleClick}/>
+              )
+          })}
+       
+        
+      </div>
     </>
   )
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const Square = (props) => {
+  const handleClick = () => {
+    props.playerSelection(props.index)
+  }
   return (
-    <div className="square"></div>
+    <>
+      <div className="square" onClick={handleClick}>
+        {props.value}
+        
+      </div>
+    </>
   )
 }
 export default Square
